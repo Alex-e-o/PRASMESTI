caad4745be24949fa5ec477af30fe7a03fb55ea1
Alex-e-o <alexisossene@gmail.com>
2026-02-04 08:22:04 -0500

    Reformat headers, remove mobile menu, fix links
    Reformatted HTML indentation in include/header.php and include/header_out.php to clean up markup (no functional changes). Removed a large commented-out mobile menu block from index.php and updated hero CTA hrefs from '/news' and '/contact' to relative 'news' and 'contact'.

--- include/header_out.php
+++ include/header_out.php
@@ -33,68 +33,68 @@
         </div>
     </div>
     <div class="sticky-wrapper">
-            <!-- Main Menu Area -->
-            <div class="container">
-                <div class="menu-area">
-                    <div class="menu-area-wrap">
-                        <div class="col-auto d-inline-block d-lg-none">
-                            <div class="header-logo">
-                                <a href="/"><img src="../assets/img/Logo.png" alt="Logo.png"></a>
-                            </div>
+        <!-- Main Menu Area -->
+        <div class="container">
+            <div class="menu-area">
+                <div class="menu-area-wrap">
+                    <div class="col-auto d-inline-block d-lg-none">
+                        <div class="header-logo">
+                            <a href="/"><img src="../assets/img/Logo.png" alt="Logo.png"></a>
                         </div>
-                        <nav class="main-menu d-none d-lg-block">
-                            <ul>
-                                <li class="active">
-                                    <a href="../" style="color: #ff5528;">Accueil</a>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="../presentation">Présentation</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="../presentation">Qu'est-ce que le PRASMESTI? </a></li>
-                                        <li><a href="../expectations">Les attentes</a></li>
-                                        <li><a href="../objectives">En résumé, les objectifs</a></li>
-                                        <li><a href="../teams">Les responsables du portail</a></li>
-                                        <li><a href="../about">Mot du president et du commissaires</a></li>
-
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">Textes Normatifs</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="#">Textes normatifs continentaux et internationaux</a></li>
-                                        <li><a href="#">Politique sectorielle en afrique centrale</a></li>
-                                        <li><a href="#">Stratégie sectorielle en afrique centrale</a></li>
-                                        <li><a href="#">Projets sectorielles en afrique centrale</a></li>
-                                        <li><a href="#">Stratégies dans les documents cadres</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">Etat de mise en oeuvres</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="#">Angola </a></li>
-                                        <li><a href="#">Burundi</a></li>
-                                        <li><a href="#">Cameroun</a></li>
-                                        <li><a href="#">Centrafrique</a></li>
-                                        <li><a href="#">République du Congo</a></li>
-                                        <li><a href="../Gabon">Gabon</a></li>
-                                        <li><a href="#">Guinée Equatoriale</a></li>
-                                        <li><a href="#">République Démocratique du Congo</a></li>
-                                        <li><a href="#">Rwanda</a></li>
-                                        <li><a href="#">Sao Tomé et Principe</a></li>
-                                        <li><a href="#">Tchad</a></li>
-                                    </ul>
-                                </li>
-                                <li>
-                                <li><a href="#">Propriété intellectuelle</a></li>
-                                </li>
-                                <li>
-                                <li><a href="#">Innovations </a></li>
-                                </li>
-                                <li><a href="../news">Actualités</a></li>
-                            </ul>
-                        </nav>
                     </div>
+                    <nav class="main-menu d-none d-lg-block">
+                        <ul>
+                            <li class="active">
+                                <a href="../" style="color: #ff5528;">Accueil</a>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="../presentation">Présentation</a>
+                                <ul class="sub-menu">
+                                    <li><a href="../presentation">Qu'est-ce que le PRASMESTI? </a></li>
+                                    <li><a href="../expectations">Les attentes</a></li>
+                                    <li><a href="../objectives">En résumé, les objectifs</a></li>
+                                    <li><a href="../teams">Les responsables du portail</a></li>
+                                    <li><a href="../about">Mot du president et du commissaires</a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Textes Normatifs</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">Textes normatifs continentaux et internationaux</a></li>
+                                    <li><a href="#">Politique sectorielle en afrique centrale</a></li>
+                                    <li><a href="#">Stratégie sectorielle en afrique centrale</a></li>
+                                    <li><a href="#">Projets sectorielles en afrique centrale</a></li>
+                                    <li><a href="#">Stratégies dans les documents cadres</a></li>
+                                </ul>
+                            </li>
+                            <li class="menu-item-has-children">
+                                <a href="#">Etat de mise en oeuvres</a>
+                                <ul class="sub-menu">
+                                    <li><a href="#">Angola </a></li>
+                                    <li><a href="#">Burundi</a></li>
+                                    <li><a href="#">Cameroun</a></li>
+                                    <li><a href="#">Centrafrique</a></li>
+                                    <li><a href="#">République du Congo</a></li>
+                                    <li><a href="../Gabon">Gabon</a></li>
+                                    <li><a href="#">Guinée Equatoriale</a></li>
+                                    <li><a href="#">République Démocratique du Congo</a></li>
+                                    <li><a href="#">Rwanda</a></li>
+                                    <li><a href="#">Sao Tomé et Principe</a></li>
+                                    <li><a href="#">Tchad</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                            <li><a href="#">Propriété intellectuelle</a></li>
+                            </li>
+                            <li>
+                            <li><a href="#">Innovations </a></li>
+                            </li>
+                            <li><a href="../news">Actualités</a></li>
+                        </ul>
+                    </nav>
                 </div>
             </div>
         </div>
-    </header>
+    </div>
+</header>
