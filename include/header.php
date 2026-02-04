e1b608b86100ce5462fcbb90f9abe93b3467289a
Alex-e-o <alexisossene@gmail.com>
2026-02-03 20:02:44 -0500

    Modifications avec papa

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
                 <div class="row justify-content-center justify-content-lg-between align-items-center gy-2">
                     <div class="col-auto d-none d-lg-block">
                         <div class="header-logo">
-                            <a href="/"><img src="../assets/img/Logo.png" alt="logo-ceeac"></a>
+                            <a href="/"><img src="assets/img/Logo.png" alt="logo-ceeac"></a>
                         </div>
                     </div>
                     <div class="col-auto d-none d-md-block">
@@ -24,7 +24,7 @@
                             <div class="info-card">
                                 <div class="box-content">
                                     <p class="box-text">Email:</p>
-                                    <h4 class="box-title"><a href="mailto:contact@prasmesti.com">contact@prasmesti.com</a></h4>
+                                    <h4 class="box-title"><a href="mailto:prasmesti@ceeac-eccas.org">prasmesti@ceeac-eccas.org</a></h4>
                                 </div>
                             </div>
                         </div>
@@ -47,22 +47,22 @@
                     <div class="menu-area-wrap">
                         <div class="col-auto d-inline-block d-lg-none">
                             <div class="header-logo">
-                                <a href="/"><img src="../assets/img/Logo.png" alt="Logo.png"></a>
+                                <a href="/"><img src="assets/img/Logo.png" alt="Logo.png"></a>
                             </div>
                         </div>
                         <nav class="main-menu d-none d-lg-block">
                             <ul>
                                 <li class="active">
-                                    <a href="/" style="color: #ff5528;">Accueil</a>
+                                    <a href="../" style="color: #ff5528;">Accueil</a>
                                 </li>
                                 <li class="menu-item-has-children">
                                     <a href="../presentation">Présentation</a>
                                     <ul class="sub-menu">
-                                        <li><a href="../presentation">Qu'est-ce que le PRASMESTI? </a></li>
-                                        <li><a href="../expectations">Les attentes</a></li>
-                                        <li><a href="../objectives">En résumé, les objectifs</a></li>
-                                        <li><a href="../teams">Les responsables du projet</a></li>
-                                        <li><a href="../about">Mot du president et du commissaires</a></li>
+                                        <li><a href="presentation/">Qu'est-ce que le PRASMESTI? </a></li>
+                                        <li><a href="expectations/">Les attentes</a></li>
+                                        <li><a href="objectives/">En résumé, les objectifs</a></li>
+                                        <li><a href="teams/">Les responsables du projet</a></li>
+                                        <li><a href="about/">Mot du president et du commissaires</a></li>
 
                                     </ul>
                                 </li>
@@ -84,7 +84,7 @@
                                         <li><a href="#">Cameroun</a></li>
                                         <li><a href="#">Centrafrique</a></li>
                                         <li><a href="#">République du Congo</a></li>
-                                        <li><a href="/Gabon">Gabon</a></li>
+                                        <li><a href="../Gabon">Gabon</a></li>
                                         <li><a href="#">Guinée Equatoriale</a></li>
                                         <li><a href="#">République Démocratique du Congo</a></li>
                                         <li><a href="#">Rwanda</a></li>
